46d524dd37f8272f296b727561a033f082732906
Chae Sang, Jung <shtingstar@hanmail.net>
2022-01-22 21:29:59 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const rotateAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0;
+    background:deeppink;
+  }
+  50% {
+    border-radius:100px;
+  }
+  0% {
+    transform:rotate(-360deg);
+    border-radius:0;
+    background:aqua;
+  }
+`;
+const WrapContent =styled.div`
+  display:flex;
+`;
+const BoxContent = styled.div`
+  display:inline-flex;
+  justify-content:center;
+  align-items: center;
+  width:100px;
+  height:100px;
+  background:${(props)=>props.color};
+`;
+const Text = styled.span`
+  color: ${(props)=>props.theme.textColor};
+  background: ${(props)=>props.theme.bgClr};
+  text-align: center;
+`;
+const BoxCircle = styled(BoxContent)`
+  border-radius:50px;
+  animation: ${rotateAnimation} 120s linear infinite;
+  transition: all 10s;
+  ${Text} {
+    transition: all 0.5s;
+  }
+  &:hover {
+    background: #f0f;
+    ${Text} {
+      font-size:40px;
+    }
+  }
+`;
+
+const Btn = styled.button`
+  background:transparent;
+  border:none;
+  background-color: #333;
+`;
+const Input = styled.input.attrs({required:true, minLength:10, disabled:true})``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <WrapContent>
+          <BoxContent color="darkred">
+            <Text>Hello~</Text>
+          </BoxContent>
+          <BoxContent color="deeppink" />
+          <BoxContent color="maroon" />
+          <BoxCircle color="olive" >
+            <Text>How are you?</Text>
+          </BoxCircle>
+      </WrapContent>
+      <Btn>Link1</Btn>
+      <Btn as="a" href="/">Link2</Btn>
+      <div>
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+      </div>
+      <div>
+
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider } from "styled-components";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  bgClr:"#111"
+}
+const lightTheme = {
+  textColor: "#333",
+  bgClr:"whitesmoke"
+}
+const defaultTheme = {
+  textColor: "#ddd",
+  bgClr:"pink"
+}
+
+let storage = "dark";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={ 
+      storage === "dark" ? darkTheme 
+      : storage === "light" ? lightTheme
+      : defaultTheme
+    }
+    >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
